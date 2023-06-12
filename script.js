e90f74b6a6eefa57b9775199c94643530f586d95
ZaeemAlam101 <101441201+ZaeemAlam99@users.noreply.github.com>
2023-06-12 18:53:38 -0500

    search bar feature added!!

--- script.js
+++ script.js
@@ -22,4 +22,61 @@ document.addEventListener('DOMContentLoaded', function() {
       document.querySelector(target).classList.add('active');
     });
   });
+
+  // Restaurant Search
+  var searchForm = document.getElementById('searchForm');
+  var searchInput = document.getElementById('searchInput');
+  var restaurantList = document.getElementById('restaurantList');
+
+  // Restaurant Data Array
+  var restaurants = [
+    { name: 'Restaurant 1', zipCode: '12345', address: '123 Main St' },
+    { name: 'Restaurant 2', zipCode: '23456', address: '456 Elm St' },
+    { name: 'Restaurant 3', zipCode: '34567', address: '789 Oak St' },
+    // Add more restaurants as needed
+  ];
+
+  searchForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var searchTerm = searchInput.value.trim().toLowerCase();
+
+    // Clear previous search results
+    restaurantList.innerHTML = '';
+
+    // Perform search based on the searchTerm
+    if (searchTerm !== '') {
+      var foundRestaurants = restaurants.filter(function(restaurant) {
+        return restaurant.zipCode === searchTerm;
+      });
+
+      if (foundRestaurants.length > 0) {
+        foundRestaurants.forEach(function(restaurant) {
+          // Create a list item for each matching restaurant
+          var listItem = document.createElement('li');
+          var nameElement = document.createElement('h3');
+          var addressElement = document.createElement('p');
+
+          nameElement.textContent = restaurant.name;
+          addressElement.textContent = restaurant.address;
+
+          listItem.appendChild(nameElement);
+          listItem.appendChild(addressElement);
+
+          // Append the list item to the restaurant list
+          restaurantList.appendChild(listItem);
+        });
+      } else {
+        // No matching restaurants found
+        var listItem = document.createElement('li');
+        listItem.textContent = 'No restaurants found';
+        restaurantList.appendChild(listItem);
+      }
+    } else {
+      // Empty search term
+      var listItem = document.createElement('li');
+      listItem.textContent = 'Enter a valid ZIP code';
+      restaurantList.appendChild(listItem);
+    }
+  });
 });
