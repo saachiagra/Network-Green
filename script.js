59eb90be7d30b776b19dae4ba65033a13eea9bfa
ZaeemAlam101 <101441201+ZaeemAlam99@users.noreply.github.com>
2023-06-12 17:35:07 -0500

    Made the website look similar to a google site
    Tried to make the "Tabs" work

--- script.js
+++ script.js
@@ -1,37 +1,25 @@
-// Function to handle form submission
-function handleSubmit(event) {
-    event.preventDefault();
-  
-    // Get the form values
-    const donorName = document.getElementById('donorName').value;
-    const donorEmail = document.getElementById('donorEmail').value;
-    const foodDescription = document.getElementById('foodDescription').value;
-  
-    // Create a new donation object
-    const donation = {
-      donorName,
-      donorEmail,
-      foodDescription,
-      timestamp: new Date().toLocaleString() // Add timestamp
-    };
-  
-    // Clear the form inputs
-    document.getElementById('donorName').value = '';
-    document.getElementById('donorEmail').value = '';
-    document.getElementById('foodDescription').value = '';
-  
-    // Display the donation in the list
-    const donationsList = document.getElementById('donations');
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `
-      <strong>${donation.donorName}</strong> (${donation.donorEmail})<br>
-      ${donation.foodDescription}<br>
-      <em>${donation.timestamp}</em>
-    `;
-    donationsList.prepend(listItem);
-  }
-  
-  // Add event listener to the form
-  const donationForm = document.getElementById('donationForm');
-  donationForm.addEventListener('submit', handleSubmit);
-  
+document.addEventListener('DOMContentLoaded', function() {
+  // Get all tab links and tab content
+  var tabLinks = document.querySelectorAll('nav a');
+  var tabContents = document.querySelectorAll('.tab-content');
+
+  // Add event listeners to the tab links
+  tabLinks.forEach(function(tabLink) {
+    tabLink.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      // Remove active class from all tab links and tab contents
+      tabLinks.forEach(function(link) {
+        link.classList.remove('active');
+      });
+      tabContents.forEach(function(content) {
+        content.classList.remove('active');
+      });
+
+      // Add active class to the clicked tab link and corresponding tab content
+      var target = this.getAttribute('href');
+      this.classList.add('active');
+      document.querySelector(target).classList.add('active');
+    });
+  });
+});
