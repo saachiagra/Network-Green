81409702ec201a2334e0c38801126f9b79cdb9cf
Psangs123 <39105705+FearlessTiger@users.noreply.github.com>
2023-06-19 11:45:15 -0500

    test codes implemented

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, jsonify
+import googlemaps
+
+app = Flask(__name__)
+
+# Enter your Google Maps API key here
+api_key = "AIzaSyDp2sWaBCBVR0P3katwTumON1JbB5Iw1aA"
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/restaurants", methods=["POST"])
+def restaurants():
+    zip_code = request.form["zipcode"]
+
+    gmaps = googlemaps.Client(key=api_key)
+    geocode_result = gmaps.geocode(zip_code)
+    lat_lng = geocode_result[0]['geometry']['location']
+    location = (lat_lng['lat'], lat_lng['lng'])
+
+    restaurants = gmaps.places_nearby(location=location, radius=500, type='restaurant')
+
+    return render_template("restaurants.html", restaurants=restaurants['results'])
+
+if __name__ == "__main__":
+    app.run(debug=True)
